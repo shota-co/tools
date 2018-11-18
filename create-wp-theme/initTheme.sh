e0bccfa0362ecf2f0ddeaf49f6a2f5d72b120e05
shota-co <dripcoffee1018@gmail.com>
2018-11-19 00:41:27 +0900

    add shell scripts

---
+++ create-wp-theme/initTheme.sh
@@ -0,0 +1,14 @@
+read THEMENAME
+mkdir $THEMENAME
+cd $THEMENAME
+touch index.php style.css
+mkdir template-part
+mkdir src
+cd src
+mkdir styles scripts images
+cd ..
+mkdir dist
+cd dist
+mkdir styles scripts images
+cd ..
+npm init -y
